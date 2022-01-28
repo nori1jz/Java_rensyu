9c824b4b6e26759748284771f1271e137a2da693
nori1jz <apriotype2xxnorixx@gmail.com>
2022-01-28 11:47:15 +0900

    [Fix]4章

--- Chapter04.java
+++ Chapter04.java
@@ -1,14 +1,11 @@
 
 public class Chapter04 {
 	public static void main(String[] args) {
-		String str1;
-		str1 = "真田幸村";
+		final int LUCKY_NUMBER = 7;
+		String str1 = "真田幸村";
 		System.out.println(str1);
-		
 		str1 = "こんにちは";
 		System.out.println(str1);
-		
-		final int LUCKY_NUMBER = 7;
 		System.out.println(LUCKY_NUMBER);
 	}
 }
