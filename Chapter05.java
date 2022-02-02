04be9eb9ae3d440ca02334ee493c61f8918698e5
nori1jz <apriotype2xxnorixx@gmail.com>
2022-02-02 18:37:44 +0900

    [Finish]5章

---
+++ Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123456L;
+		double double1 = 12345.6;
+		boolean boolean1 = false;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 123.4f;
+		Integer integer1 = 7;
+		Character char1 = 'A';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(char1);
+		
+		int[] abc = { 12, 14, 17, 98, 90 };
+		System.out.println(abc[0]);
+		
+	}
+}
